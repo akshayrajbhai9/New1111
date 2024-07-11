a8fe55b9f166927f5221f1c7964d2f04cfc85208
pkvgithub <145007232+pkvgithub@users.noreply.github.com>
2024-07-11 20:55:16 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@ import os
 
 api_id = "25129557"
 api_hash = "83c9546cfdee154fd2d16759c4d0582a"
-bot_token = "7014436725:AAH1uW3D79o0vMmphVgGE_1LQwIFxUj7pZY"
+bot_token = "7054905481:AAENHlnIC95Z7mFK_OqurekYW8l9geJJRf0"
