f40ef5cf6eafb168bd836150038f3819fc2e96dd
legendofficial12 <skphotograph029@gmail.com>
2025-01-31 19:18:45 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@ import os
 
 api_id = "29500466"
 api_hash = "97de63084b44bb7ca5ab11d1252fa098"
-bot_token = "7416049990:AAFMpt3Dgus8ZsvUwuK5-FAGtwqj27Pwq4c"
+bot_token = "7446742611:AAGxooqYbqXuSF_7Jn0HGQKmv1Km3-K3UXw"
