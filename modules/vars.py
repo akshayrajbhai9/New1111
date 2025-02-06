038560f257568455a387d152c30e4e774fb575cb
akshayrajbhai9 <akshayrajbhai999@gmail.com>
2025-02-06 12:51:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@ import os
 
 api_id = "15816419"
 api_hash = "626ed6dab78881858778d9663682962e"
-bot_token = "7052472056:AAFDjscYXK8KfREHCKelg9FCr4_m7hXKJHY"
+bot_token = "6457620096:AAG9jsXG7t_gGTKh8R74UIf-pe-Zt6caMo0"
