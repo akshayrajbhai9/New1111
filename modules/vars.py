f4e14030350dd25e680bcdd6c876b414fc67013c
akshayrajbhai9 <akshayrajbhai999@gmail.com>
2025-02-10 22:44:59 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -2,4 +2,4 @@ import os
 
 api_id = "15816419"
 api_hash = "626ed6dab78881858778d9663682962e"
-bot_token = "6457620096:AAG9jsXG7t_gGTKh8R74UIf-pe-Zt6caMo0"
+bot_token = "6861339015:AAHqPjkTipRLyTW2zEPuC9OE3tVlY2oXeRM"
